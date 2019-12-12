ac9cda0a8e784088fce081ca725f22a566559613
guhao022 <378999587@qq.com>
2019-12-12 15:36:43 +0800

    fix

--- novel/novel.go
+++ novel/novel.go
@@ -1,26 +1,168 @@
 package main
 
 import (
-	"bytes"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/extensions"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/text/encoding/simplifiedchinese"
-	"io/ioutil"
-	"math/rand"
-	"net"
 	"net/http"
 	"os"
 	"path"
 	"regexp"
 	"time"
 	"strings"
+	"strconv"
+	"net"
+	"github.com/ghaoo/rbootx/tools"
+	"github.com/PuerkitoBio/goquery"
+	"fmt"
+	"encoding/json"
+	"path/filepath"
+	"bufio"
+	"io"
 )
 
-const BOOK_STORE = `E:\data\book`
-//const BOOK_STORE = `./book`
+var url = `https://www.bqg5200.com/xiaoshuo/23/23730/`
+
+type Catalog struct {
+	ID          int       // ID
+	SubID       string    // SUB ID
+	Name        string    // 名称
+	Author      string    // 作者
+	Url         string    // 链接
+	Chapters    []Chapter // 章节ID列表
+	Category    string    // 类别
+	LastChapter string    // 最新章节
+	LastUpdate  string    // 最后更新
+}
+
+type Chapter struct {
+	ID   int
+	Url  string
+	Name string
+}
 
 func main() {
+	cat := GetCatalog(url)
+
+	fetchContent(&cat)
+
+	path := cat.Name
+
+	fileMerge(path)
+}
+
+func GetCatalog(url string) Catalog {
+	cl := Catalog{}
+
+	logrus.Warn(url)
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.bqg5200.com"),
+	)
+
+	c.Limit(&colly.LimitRule{
+		Parallelism: 1,
+		RandomDelay: 5 * time.Second,
+	})
+
+	c.WithTransport(&http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	})
+
+	extensions.RandomUserAgent(c)
+
+	var reg = regexp.MustCompile(`https:\/\/www.bqg5200.com\/xiaoshuo\/(\d+)\/(\d+)[\/]?$`)
+	var reg2 = regexp.MustCompile(`https:\/\/www.bqg5200.com\/xiaoshuo\/\d+\/\d+\/(\d+).html`)
+	c.OnHTML("div#maininfo", func(e *colly.HTMLElement) {
+		url := e.Request.URL.String()
+
+		idr := reg.FindStringSubmatch(url)
+
+		subid := idr[1]
+
+		idstr := idr[2]
+
+		id, _ := strconv.Atoi(idstr)
+
+		h, _ := e.DOM.Html()
+
+		html, _ := tools.DecodeGBK([]byte(h))
+
+		dom := e.DOM.SetHtml(string(html))
+
+		title := dom.Find("div.coverecom div:nth-of-type(2)")
+
+		name := title.Find("h1").Text()
+
+		author := title.Find("span:first-of-type").Text()
+
+		category := title.Find("span:nth-of-type(2) a").Text()
+
+		last_update := title.Find("span:nth-of-type(3)").Text()
+
+		last_chapter := dom.Find("#readerlist ul li:last-of-type a").Text()
+
+		cpts := []Chapter{}
+		dom.Find("#readerlist ul li").Each(func(i int, s *goquery.Selection) {
+
+			cname := s.Find("a").Text()
+			curl, _ := s.Find("a").Attr("href")
+			curl = e.Request.AbsoluteURL(curl)
+
+			if reg2.MatchString(curl) {
+				cid, err := strconv.Atoi(reg2.FindStringSubmatch(curl)[1])
+
+				if err != nil {
+					cid = 0
+				}
+
+				cpt := Chapter{
+					ID:   cid,
+					Name: cname,
+					Url:  curl,
+				}
+
+				cpts = append(cpts, cpt)
+			}
+
+		})
+
+		cl.ID = id
+		cl.SubID = subid
+		cl.Name = name
+		cl.Author = author
+		cl.Url = url
+		cl.Category = category
+		cl.Chapters = cpts
+		cl.LastChapter = last_chapter
+		cl.LastUpdate = last_update
+
+		fname := path.Join(os.Getenv("BOOK_PATH"), name, "data.json")
+
+		data, err := json.Marshal(&cl)
+		if err != nil {
+			logrus.Error(err)
+		} else {
+			tools.FileWrite(fname, data)
+		}
+
+	})
+
+	c.Visit(url)
+
+	c.Wait()
+
+	return cl
+}
+
+func fetchContent(cl *Catalog) {
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.bqg5200.com"),
@@ -28,17 +170,14 @@ func main() {
 		colly.Async(true),
 	)
 
+	c.Limit(&colly.LimitRule{
+		DomainRegexp: "www.bqg5200.com/*",
+		Parallelism:  30,
+		RandomDelay:  5 * time.Second,
+	})
+
 	c.WithTransport(&http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			//KeepAlive: 30 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
+		DisableKeepAlives: true,
 	})
 
 	extensions.RandomUserAgent(c)
@@ -53,11 +192,9 @@ func main() {
 
 		h, _ := e.DOM.Html()
 
-		html, _ := DecodeGBK([]byte(h))
+		html, _ := tools.DecodeGBK([]byte(h))
 
 		dom := e.DOM.SetHtml(string(html))
-
-		class_name := dom.Find("#header .readNav :nth-child(2)").Text()
 
 		book_name := dom.Find("#header .readNav :nth-child(3)").Text()
 
@@ -70,9 +207,9 @@ func main() {
 
 		content := "### " + title + "\n" + article + "\n\n"
 
-		filepath := path.Join(class_name, book_name, fname[1])
+		fpath := filepath.Join(book_name, fname[1]+".rbx")
 
-		err := write(filepath, content)
+		err := tools.FileWrite(fpath, []byte(content))
 
 		if err != nil {
 			logrus.Errorf("%v\n", err)
@@ -80,73 +217,72 @@ func main() {
 
 	})
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Request.AbsoluteURL(e.Attr("href"))
-
-		//c.Visit(link)
-
-		if strings.HasPrefix(link, `https://www.bqg5200.com/book`) || strings.HasPrefix(link, `https://www.bqg5200.com/xiaoshuo`) {
-			c.Visit(link)
-		}
-	})
-
-	c.Limit(&colly.LimitRule{
-		RandomDelay:  2 * time.Second,
-		Parallelism:  5,
-	})
-
 	c.OnRequest(func(r *colly.Request) {
-		time.Sleep(getRandomDelay(10000))
-		logrus.Infof("Visiting %s", r.URL.String())
+		time.Sleep(time.Second)
+		logrus.Debugf("Visiting %s", r.URL.String())
 	})
 
-	c.Visit("https://www.bqg5200.com")
+	for _, cpt := range cl.Chapters {
+
+		c.Visit(cpt.Url)
+	}
 
 	c.Wait()
 
 }
 
-func write(file, content string) error {
+func fileMerge(root string) error {
+	name := filepath.Base(root)
 
-	filepath := path.Join(BOOK_STORE, file)
+	out_name := filepath.Join(root, name+".txt")
 
-	basepath := path.Dir(filepath)
-	// 检测文件夹是否存在   若不存在  创建文件夹
-	if _, err := os.Stat(basepath); err != nil {
-
-		if os.IsNotExist(err) {
-
-			err = os.MkdirAll(basepath, os.ModePerm)
-
-			if err != nil {
-				return err
-			}
-		} else {
-			return err
-		}
-	}
-
-	data := []byte(content)
-
-	f, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR, os.ModePerm)
+	out_file, err := os.OpenFile(out_name, os.O_CREATE|os.O_WRONLY, 0777)
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Can not open file %s", out_name)
 	}
 
-	_, err = f.Write(data)
+	bWriter := bufio.NewWriter(out_file)
 
-	return err
-}
+	bWriter.Write([]byte("## " + name + "\n\n\n"))
 
-func getRandomDelay(seed int64) time.Duration {
-	return time.Duration(rand.Int63n(seed+1000)) * time.Millisecond
-}
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 
-func DecodeGBK(s []byte) ([]byte, error) {
-	reader := simplifiedchinese.GB18030.NewDecoder().Reader(bytes.NewReader(s))
+		if !info.IsDir() && strings.HasSuffix(path, ".rbx") {
+			//logrus.Printf("读取文件：%s \n", info.Name())
 
-	return ioutil.ReadAll(reader)
+			fp, err := os.Open(path)
+
+			if err != nil {
+				fmt.Printf("Can not open file %v", err)
+				return err
+			}
+
+			defer fp.Close()
+
+			bReader := bufio.NewReader(fp)
+
+			for {
+
+				buffer := make([]byte, 1024)
+				readCount, err := bReader.Read(buffer)
+				if err == io.EOF {
+					break
+				} else {
+					bWriter.Write(buffer[:readCount])
+				}
+
+			}
+
+			bWriter.Write([]byte("\n\n"))
+		}
+
+		return err
+	})
+
+	bWriter.Flush()
+
+	return nil
 }
 
 func init() {
